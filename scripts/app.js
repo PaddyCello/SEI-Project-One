47984497563a36486373b80325ee218817198872
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-02-18 22:54:50 +0000

    assign global variables for grid

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,12 @@ function init() {
   const width = 9
   const height = 9
   const gridArea = width * height
+  const gridWrapper = document.querySelector('.grid-wrapper')
   const cells = []
 
   function makeGrid(gridArea) {
     for (let i = 0; i < gridArea; i++) {
+      const cell = document.createElement('div')
       
     }
   }
