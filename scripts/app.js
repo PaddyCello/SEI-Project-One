a25a0aabfbc1e80f59e1cae29e1e20ae9c810721
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-02-18 22:24:28 +0000

    make div for grid wrapper

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+function init() {
+
+  console.log('Javascript connected')
+
+  const width = 9
+  const height = 9
+  const gridArea = width * height
+  const cells = []
+
+  function makeGrid(gridArea) {
+    for (let i = 0; i < gridArea; i++) {
+      
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
