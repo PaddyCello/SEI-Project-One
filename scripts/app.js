164f93dd04d78719cb0afe6a7112aa9c3c404a0d
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-02-19 11:09:02 +0000

    making config array for rectangle piece

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,10 @@ function init() {
   const gridArea = width * height
   const gridWrapper = document.querySelector('.grid-wrapper')
   const cells = []
+  const startPosition = cells[4]
+  let currentPosition = startPosition
+
+  const rectangle = [cells[currentPosition], cells[currentPosition + width], cells[currentPosition + (width * 2)], cells[currentPosition + (width * 3)]]
 
   function makeGrid(gridArea) {
     for (let i = 0; i < gridArea; i++) {
