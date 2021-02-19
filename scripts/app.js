2c65d0b45cec7d7f1a28bac0c3a3bab5ea1844be
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-02-19 21:53:10 +0000

    ending the day with questions

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,52 @@ function init() {
 
   console.log('Javascript connected')
 
-  //* GRID CONSTRUCTOR
+  
   const width = 9
   const height = 9
   const gridArea = width * height
   const gridWrapper = document.querySelector('.grid-wrapper')
   const cells = []
-  let startPosition = 40
+  const startPosition = 40
   let currentPos = startPosition
   document.addEventListener('keyup', processShape)
 
+  //* ARRAYS OF SHAPE CONFIGURATIONS
+  const rectangleN = [currentPos, (currentPos + width), (currentPos + (width * 2)), (currentPos + (width * 3))]
+  const squareN = [currentPos, (currentPos + 1), (currentPos + width), (currentPos + width + 1)]
+  const sidewaysTN = [currentPos, (currentPos + width), (currentPos + (width * 2)), (currentPos + width + 1)]
+  const zigZagN = [(currentPos), (currentPos + width), (currentPos + 1 + (width * 2)), (currentPos + width + 1)]
+ 
+  const rectangleE = [currentPos, (currentPos - 1), (currentPos - 2), (currentPos - 3)]
+  const squareE = [currentPos, (currentPos - 1), (currentPos + width - 1), (currentPos + width)]
+  const sidewaysTE = [currentPos, (currentPos - 1), (currentPos + width - 1), (currentPos - 2)]
+  const zigZagE = [currentPos, (currentPos - 1), (currentPos + width - 1), (currentPos + width - 2)]
+ 
+  const rectangleS = [currentPos, (currentPos - width), (currentPos - (width * 2)), (currentPos - (width * 3))]
+  const squareS = [currentPos, (currentPos - 1), (currentPos - width), (currentPos - (width + 1))]
+  const sidewaysTS = [currentPos, (currentPos - width), (currentPos - (width * 2)), (currentPos - (width + 1))]
+  const zigZagS = [currentPos, (currentPos - width), (currentPos - 1 - (width * 2)), (currentPos - (width + 1))]
+ 
+  const rectangleW = [currentPos, (currentPos + 1), (currentPos + 2), (currentPos + 3)]
+  const squareW = [currentPos, (currentPos + 1), (currentPos - width), (currentPos - (width - 1))]
+  const sidewaysTW = [currentPos, (currentPos + 1), (currentPos + 2), (currentPos - (width - 1))]
+  const zigZagW = [currentPos, (currentPos - (width - 1)), (currentPos + 1), (currentPos - (width - 2))]
+ 
+  //* ARRAYS OF SHAPES IN ALL ROTATIONS
+  const rectangleArray = [rectangleN, rectangleE, rectangleS, rectangleW]
+  const squareArray = [squareN, squareE, squareS, squareW]
+  const sidewaysTArray = [sidewaysTN, sidewaysTE, sidewaysTS, sidewaysTW]
+  const zigZagArray = [zigZagN, zigZagE, zigZagS, zigZagW]
+
+  const starterArray = [rectangleArray, squareArray, sidewaysTArray, zigZagArray]
+
+  //* ASSIGN VARIABLES TO CHOSEN SHAPES
+  let currentArray = rectangleArray
+  let currentIndex = 0
+  let currentShape = currentArray[currentIndex]
+
+ 
+  //* GRID CONSTRUCTOR
 
   function makeGrid() {
     for (let i = 0; i < gridArea; i++) {
@@ -21,43 +57,19 @@ function init() {
       gridWrapper.appendChild(cell)
       cells.push(cell)
     }
+    addShape(currentShape, startPosition)
   }
   makeGrid(currentPos)
 
-  //* ARRAYS OF SHAPE CONFIGURATIONS
-  const rectangleN = [currentPos, (currentPos + width), (currentPos + (width * 2)), (currentPos + (width * 3))]
-  const squareN = [currentPos, (currentPos + 1), (currentPos + width), (currentPos + width + 1)]
-  const sidewaysTN = [currentPos, (currentPos + width), (currentPos + (width * 2)), (currentPos + width + 1)]
-  const zigZagN = [(currentPos), (currentPos + width), (currentPos + 1 + (width * 2)), (currentPos + width + 1)]
-
-  const rectangleE = [currentPos, (currentPos - 1), (currentPos - 2), (currentPos - 3)]
-  const squareE = [currentPos, (currentPos - 1), (currentPos + width - 1), (currentPos + width)]
-  const sidewaysTE = [currentPos, (currentPos - 1), (currentPos + width - 1), (currentPos - 2)]
-  const zigZagE = [currentPos, (currentPos - 1), (currentPos + width - 1), (currentPos + width - 2)]
-
-  const rectangleS = [currentPos, (currentPos - width), (currentPos - (width * 2)), (currentPos - (width * 3))]
-  const squareS = [currentPos, (currentPos - 1), (currentPos - width), (currentPos - (width + 1))]
-  const sidewaysTS = [currentPos, (currentPos - width), (currentPos - (width * 2)), (currentPos - (width + 1))]
-  const zigZagS = [currentPos, (currentPos - width), (currentPos - 1 - (width * 2)), (currentPos - (width + 1))]
-
-  const rectangleW = [currentPos, (currentPos + 1), (currentPos + 2), (currentPos + 3)]
-  const squareW = [currentPos, (currentPos + 1), (currentPos - width), (currentPos - (width - 1))]
-  const sidewaysTW = [currentPos, (currentPos + 1), (currentPos + 2), (currentPos - (width - 1))]
-  const zigZagW = [currentPos, (currentPos - (width - 1)), (currentPos + 1), (currentPos - (width - 2))]
-
-  //* ARRAYS OF SHAPES IN ALL ROTATIONS
-  const rectangleArray = [rectangleN, rectangleE, rectangleS, rectangleW]
-  const squareArray = [squareN, squareE, squareS, squareW]
-  const sidewaysTArray = [sidewaysTN, sidewaysTE, sidewaysTS, sidewaysTW]
-  const zigZagArray = [zigZagN, zigZagE, zigZagS, zigZagW]
-
-  //* ASSIGN VARIABLES TO CHOSEN SHAPES
-  let currentArray = rectangleArray
-  let currentIndex = 0
-  const currentShape = currentArray[currentIndex]
-
+  //* MIGHT BE USEFUL LATER
+  // function generateShape(array, position) {
+  //   currentShape = array[Math.floor(Math.random() * 4)][0]
+  // }
+  // generateShape(starterArray, currentPos)
+  //*
+ 
   //* GET AND REMOVE SHAPES
-  function addShape(array) {
+  function addShape(array, position) {
     array.forEach(item => {
       cells[item].classList.add('rectangle')
     })
@@ -69,7 +81,7 @@ function init() {
       cells[item].classList.remove('rectangle')
     })
   } 
-  addShape(currentShape)
+  
   
   //* ROTATOR FUNCTION
   function turnShape() {
@@ -84,14 +96,20 @@ function init() {
     }  
   }
   //* RIGHT MOVE FUNCTION (IN PROGRESS)
-  function moveRight(array) {
-    array.forEach(item => {
+  function moveRight(currentShape) {
+    removeShape(currentShape)
+    currentPos++
+    return currentShape.forEach(item => {
+      console.log(item)
       item++
+      console.log(item)
       cells[item].classList.add('rectangle')
     })
   }
   //* LEFT MOVE FUNCTION (IN PROGRESS)
   function moveLeft(array) {
+    removeShape(currentShape)
+    currentPos--
     array.forEach(item => {
       item--
       cells[item].classList.add('rectangle')
@@ -99,6 +117,8 @@ function init() {
   }
   //* MOVE DOWN FUNCTION (IN PROGRESS)
   function moveDown(array) {
+    removeShape(currentShape)
+    currentPos += width
     array.forEach(item => {
       item += width
       cells[item].classList.add('rectangle')
@@ -109,16 +129,10 @@ function init() {
     if (event.keyCode === 32) {
       turnShape() 
     } else if (event.keyCode === 39) {
-      removeShape(currentShape)
-      currentPos++
       moveRight(currentShape)
     } else if (event.keyCode === 37) {
-      removeShape(currentShape)
-      currentPos--
       moveLeft(currentShape)
     } else if (event.keyCode === 40) {
-      removeShape(currentShape)
-      currentPos += width
       moveDown(currentShape)
     } else {
       console.log('invalid key')
