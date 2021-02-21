9e3de2f119f4bbf388c351f417523020a0751fdb
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-02-21 10:08:09 +0000

    shapes update in rotator function

--- scripts/app.js
+++ scripts/app.js
@@ -8,9 +8,11 @@ function init() {
   const gridArea = width * height
   const gridWrapper = document.querySelector('.grid-wrapper')
   const cells = []
-  const startPosition = Math.floor(width / 2)
+  const startPosition = Math.floor(width / 2) * 10
   let currentPos = startPosition
   let currentShape
+  let currentArray
+  let currentIndex
   let nextShape
   let score = 0
   const scoreBoard = document.querySelector('.score')
@@ -48,11 +50,7 @@ function init() {
   const zigZagArray = [zigZagN, zigZagE, zigZagS, zigZagW]
 
   const starterArray = [rectangleArray, squareArray, sidewaysTArray, zigZagArray]
-
-  //* ASSIGN VARIABLES TO CHOSEN SHAPES
-  let currentArray = rectangleArray
-  let currentIndex = 0
- 
+  
   //* GRID CONSTRUCTOR
 
   function makeGrid() {
@@ -69,7 +67,9 @@ function init() {
   
   //* GENERATE NEXT SHAPE
   function createShape(array, position) {
-    nextShape = array[Math.floor(Math.random() * array.length)][0]
+    currentIndex = 0
+    currentArray = array[Math.floor(Math.random() * array.length)]
+    nextShape = currentArray[currentIndex]
     currentPos = position
     return nextShape
   }
@@ -92,13 +92,15 @@ function init() {
   //* ROTATOR FUNCTION (IN PROGRESS)
   function turnShape() {
     if (currentIndex < 3) {
-      removeShape(currentArray[currentIndex])
+      removeShape(currentShape)
       currentIndex++
-      addShape(currentArray[currentIndex])
+      currentShape = currentArray[currentIndex]
+      addShape(currentShape)
     }  else {
-      removeShape(currentArray[currentIndex])
+      removeShape(currentShape)
       currentIndex = 0
-      addShape(currentArray[currentIndex])
+      currentShape = currentArray[currentIndex]
+      addShape(currentShape)
     }
   }
   //* RIGHT MOVE FUNCTION
